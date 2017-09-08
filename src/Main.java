1375e0ff381867452597f79b1c96051b77cb91b5
Zach Connelly <zmconnelly@gmail.com>
2017-09-07 20:55:29 -0600

    stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello wurld");
+        File file = new File(args[0]);
+        String[] formattedLines = convertFileToARFF(file);
+        writeToFile("path", formattedLines);
+    }
+
+    private static String[] convertFileToARFF(File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            return bufferedReader.lines()
+                    .map(Main::formatLine)
+                    .toArray(String[]::new);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String formatLine(String line) {
+        String formattedLine = "";
+        // Parse individual line, return in ARFF format
+        return formattedLine;
+    }
+
+    private static void writeToFile(String path, String[] lines) {
+        // Create .arff file, write lines to it
     }
 }
