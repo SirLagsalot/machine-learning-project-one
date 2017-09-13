57f0a1938d723c1fcc548e24e305eee20c1b80ff
Zach Connelly <zmconnelly@gmail.com>
2017-09-12 20:52:57 -0600

    changes by me

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,73 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
-public class Main {
+public class ARFFConverter {
 
+    private static final String ATTRIBUTE_TAG = "@ATTRIBUTE";
+    private static final String DATA_TAG = "@DATA";
+    private static final String RELATION_TAG = "@RELATION";
+
+    /**
+     * args[1]: Name of dataset
+     * args[2]: List position of classification attribute; {first, last}
+     */
     public static void main(String[] args) {
-        File namesFile = new File(args[0]);
-        File dataFile = new File(args[1]);
-        String[] header = {}, data = {};
-        try {
-            header = parseHeader(namesFile);
-            data = parseData(dataFile);
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+        File metadataFile = new File(args[0].concat(".header"));
+        File dataFile = new File(args[0].concat(".data"));
+        boolean classLblIsFirst = Objects.equals(args[1], "first");
+
+        Stream<String> metaDataStream = getFileStream(metadataFile);
+        Stream<String> dataStream = getFileStream(dataFile);
+
+        String headerInfo = getHeaderInfo(metaDataStream);
+        String relationInfo = getRelationInfo(metaDataStream);
+        String attributeInfo = getAttributeInfo(metaDataStream);
+        String dataInfo = getDataInfo(dataStream);
+
+        if (null != dataStream) {
+            dataStream
+                    .filter(line -> !line.isEmpty())
+                    .map(line -> convertLineToARFF(line, classLblIsFirst))
+                    .toArray(String[]::new);
+        } else {
             System.exit(-1);
-        }
-
-
-        File arffFile = buildArffFile(args[0].matches("*\\."), header, data);
-        writeFile("path", arffFile);
-    }
-
-    private static String[] parseHeader(File headerFile) {
-        Stream<String> lines = getFileStream(headerFile);
-        if (lines != null) {
-            String[] formattedLines = {};
-            // Parse header
-            return formattedLines;
-        } else {
-            return null;
-        }
-    }
-
-    private static String[] parseData(File dataFile) throws FileNotFoundException {
-        Stream<String> lines = getFileStream(dataFile);
-        if (lines != null) {
-            String[] formattedLines = {};
-            // Parse header
-            return formattedLines;
-        } else {
-            return null;
         }
     }
 
     private static Stream<String> getFileStream(File file) {
         try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            return bufferedReader.lines().map(Main::formatLine);
+            return new BufferedReader(new FileReader(file)).lines();
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
             return null;
         }
     }
 
-    private static String formatLine(String line) {
-        String formattedLine = "asdf";
-        // Parse individual line, return in ARFF format
-        return formattedLine;
+    private static String getHeaderInfo(Stream<String> metadata) {
+        return "";
+    }
+
+    private static String getRelationInfo(Stream<String> metadata) {
+        return "";
+    }
+
+    private static String getAttributeInfo(Stream<String> metadata) {
+        return "";
+    }
+
+    private static String getDataInfo(Stream<String> metadata) {
+        return "";
+    }
+
+    private static String convertLineToARFF(String line, boolean first) {
+        List<String> features = Arrays.asList(line.split(","));
+        String classification = first ? features.remove(0) : features.remove(features.size() - 1);
+        return "";
     }
 
     private static File buildArffFile(String fileName, String[] header, String[] data) {
